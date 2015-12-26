6282be3ea24738c6153b06d669822b5059c6ef12
ruth.mesfun <ruth.mesfun@flatironschool.com>
2015-12-25 22:04:06 -0500

    All Done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # Write code here
+#require 'pry'
+class School
+  attr_accessor :school_name
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = Hash.new
+
+  end
+
+  def add_student(student, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = Array.new
+      @roster[grade] << student
+    end
+  end
+  
+  def roster
+    @roster
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      @roster[grade]=students.sort
+    end
+  end
+
+end
+
+#binding.pry
